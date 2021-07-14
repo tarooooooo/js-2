e7d4af8d30ec04bd274f0cdf843e95d70a1ff425
RyutaroMaeda <ryutaromaeda83@gmail.com>
2021-07-14 11:44:51 +0900

    [finish]js-2

---
+++ app.js
@@ -0,0 +1,77 @@
+// let hello ='Hello World';
+// alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+
+// // 負数を代入する
+// let int2 = -10;
+// alert(int2);
+// // 小数点を代入する
+// let float1 = 3.14;
+// alert(float1);
+
+// // 文字列を代入する
+// let str1 = 'JavaScriptを覚えよう';
+// alert(str1);
+
+// alert(4+3);
+
+// 引き算
+// alert(8 - 5);
+
+// // 掛け算
+// alert(2 * 6);
+
+// // 割り算
+// alert(10 / 2);
+
+// alert('Hello'+'World');
+
+
+
+// let str1='Hello';
+// let str2='World!!';
+// alert(str1 + str2);
+// let orange = 100;
+// let apple = 120;
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// }else if(orange == apple){
+//   alert('みかんとりんごは同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num*2;
+//   count = count+1
+// }
+
+// alert('2を掛け続けて'+max+'を超えるのに必要だった回数は'+count+'回です');
+
+// let i;
+// let num=0;
+
+// for(i=1;i<11;i++){
+//   num = num+i;
+// }
+
+// alert('１から１０まで足し算した結果は' + num + 'です');
+
+// 確認問題
+let num1 = 1;
+let num2 = 2;
+
+// 足し算
+alert(num1+num2);
+// 引き算
+alert(num1-num2);
+// 掛け算
+alert(num1*num2);
+// 割り算
+alert(num1/num2);
